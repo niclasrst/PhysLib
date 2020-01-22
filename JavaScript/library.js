624c57505c68c239851bbad09863946073397865
anikinil <anikins.ilja@gmail.com>
2020-01-22 01:26:44 +0100

    starting at given quantities

--- JavaScript/library.js
+++ JavaScript/library.js
@@ -1,6 +1,7 @@
 const math = require('mathjs');
 
-let treePath = [];
+// let treePath = [];
+
 
 class Quantity {
 	constructor (name, unit, value, formula, parents) {
@@ -11,7 +12,7 @@ class Quantity {
 		this.parents = parents;
 	};
 
-	canCalculate = () => {
+	cancalculateAndSetValue = () => {
 		for (var i in this.formula) {
 			if (this.formula[i].allSubQuantitiesKnown()) { return true; }
 		}
@@ -34,23 +35,19 @@ class MulFormula {
 		this.subQuantities = subQuantities;
 	}
 	getOperator = () => { return 'mul'; }
-	allSubQuantitiesKnown = () => {
+	unknownSubQuantities = () => {
+		let count = 0;
 		for (var i in this.subQuantities) {
-			if (this.subQuantities[i].value === null) {
-				return false;
-			}
-			return true;
+			if (this.subQuantities[i].value === null) { count++; }
+		}
+		return count;
+	}
+	getUnknownSubQuantity = () => {
+		for (var i in this.subQuantities) {
+			if (this.subQuantities[i].value === null) { return this.subQuantities[i]; }
 		}
 	}
-	onlyThisSubQuantityUnknown = (subQuantity) => {
-		for (var i in this.subQuantities) {
-			if (this.subQuantities[i] != subQuantity && this.subQuantities[i] == undefined) {
-				return false;
-			}
-		}
-		return true;
-	}
-	calculate = (unknownQuantity) => {
+	calculateAndSetValue = (unknownQuantity) => {
 
 		this.parentQuantity = eval(this.parentQuantity);
 
@@ -59,7 +56,7 @@ class MulFormula {
 			for (var i in this.subQuantities) {
 				calc *= this.subQuantities[i].value;
 			}
-			return calc;
+			unknownQuantity.value =  calc;
 		} else if (this.subQuantities.includes(unknownQuantity)) {
 			let calc = this.parentQuantity.value;
 			for (var i in this.subQuantities) {
@@ -67,15 +64,14 @@ class MulFormula {
 					calc /= this.subQuantities[i].value;
 				}
 			}
-
-			return calc;
+			unknownQuantity.value =  calc;
 		} else {
 			throw new Error('Die angegebene Größe ' + unknownQuantity.name + ' ist nicht in der MulFormula vorhanden.')
 		}
 	}
-	setParentValue = (parentValue) => {
-		eval(this.parentQuantity).value = parentValue;
-	}
+	// setParentValue = (parentValue) => {
+	// 	eval(this.parentQuantity).value = parentValue;
+	// }
 }
 
 class AddFormula {
@@ -84,29 +80,25 @@ class AddFormula {
 		this.subQuantities = subQuantities;
 	}
 	getOperator = () => { return 'add'; }
-	allSubQuantitiesKnown = () => {
+	unknownSubQuantities = () => {
+		let count = 0;
 		for (var i in this.subQuantities) {
-			if (this.subQuantities[i].value === null) {
-				return false;
-			}
-			return true;
+			if (this.subQuantities[i].value === null) { count++; }
+		}
+		return count;
+	}
+	getUnknwonSubQuantity = () => {
+		for (var i in this.subQuantities) {
+			if (this.subQuantities[i].value === null) { return this.subQuantities[i]; }
 		}
 	}
-	onlyThisSubQuantityUnknown = (subQuantity) => {
-		for (var i in this.subQuantities) {
-			if (this.subQuantities[i] != subQuantity && this.subQuantities[i] == undefined) {
-				return false;
-			}
-		}
-		return true;
-	}
-	calculate = (unknownQuantity) => {
+	calculateAndSetValue = (unknownQuantity) => {
 		if (unknownQuantity == this.parentQuantity) {
 			let calc = 0;
 			for (var i in this.subQuantities) {
 				calc += this.subQuantities[i].value;
 			}
-			return calc;
+			unknownQuantity.value =  calc;
 		} else if (this.subQuantities.includes(unknownQuantity)) {
 			let calc = this.parentQuantity.value;
 			for (var i in this.subQuantities) {
@@ -114,14 +106,14 @@ class AddFormula {
 					calc -= this.subQuantities[i].value;
 				}
 			}
-			return calc;
+			unknownQuantity.value =  calc;
 		} else {
 			throw new Error('Die angegebene Größe ' + unknownQuantity.name + ' ist nicht in der AddFormula vorhanden.')
 		}
 	}
-	setParentValue = (parentValue) => {
-		this.parentQuantity.value = parentValue;
-	}
+	// setParentValue = (parentValue) => {
+	// 	this.parentQuantity.value = parentValue;
+	// }
 }
 
 class DivFormula {
@@ -131,112 +123,109 @@ class DivFormula {
 		this.divisorQuantity = divisorQuantity;
 	}
 	getOperator = () => { return 'div'; }
-	allSubQuantitiesKnown = () => {
-		for (var i in this.subQuantities) {
-			if (this.subQuantities[i].value === null) {
-				return false;
-			}
-			return true;
-		}
+	unknownSubQuantities = () => {
+		let count = 0;
+		if (this.dividendQuantity.value === null) { count++; }
+		if (this.divisorQuantity.value === null) { count++; }
+		return count;
 	}
-	onlyThisSubQuantityUnknown = (subQuantity) => {
-		if (this.dividendQuantity == subQuantity && this.divisorQuantity != undefined ||
-			this.divisorQuantity == subQuantity && this.dividendQuantity != undefined) {
-			return false;
-		}
-		return true;
+	getUnknwonSubQuantity = () => {
+		if (this.dividendQuantity.value === null) { return this.dividendQuantity; }
+		if (this.divisorQuantity.value === null) { return this.divisorQuantity; }
 	}
-	calculate = (unknownQuantity) => {
+	calculateAndSetValue = (unknownQuantity) => {
 		if (unknownQuantity == this.parentQuantity) {
-			return eval(this.dividendQuantity).value / eval(this.divisorQuantity).value;
+			unknownQuantity.value = eval(this.dividendQuantity).value / eval(this.divisorQuantity).value;
 		} else if (unknownQuantity == this.dividendQuantity) {
-			return eval(this.parentQuantity).value * eval(this.divisorQuantity).value;
+			unknownQuantity.value = eval(this.parentQuantity).value * eval(this.divisorQuantity).value;
 		} else if (unknownQuantity == this.divisorQuantity) {
-			return eval(this.parentQuantity).value / eval(this.dividendQuantity).value;
+			unknownQuantity.value =  eval(this.parentQuantity).value / eval(this.dividendQuantity).value;
 		} else {
 			throw new Error('Die angegebene Größe ' + unknownQuantity.name + ' ist nicht in der DivFormula vorhanden.')
 		}
 	}
-	setParentValue = (parentValue) => {
-		this.parentQuantity.value = parentValue;
-	}
+	// setParentValue = (parentValue) => {
+	// 	this.parentQuantity.value = parentValue;
+	// }
 }
 
 let W_el, F_el, E_el, l, q, mgs, m, g, s;
 
-m = new Quantity (
-	'Masse',
-	'kg',
-	null,
-	[],
-	[new Parent('mgs', 0, 'mul')]
-);
 
-g = new Quantity (
-	'Fallbeschleunigung',
-	'm/s²',
-	null,
-	[],
-	[new Parent('mgs', 0, 'mul')]
-);
+{ 	m = new Quantity (  // Quantities
+		'Masse',
+		'kg',
+		null,
+		[],
+		[new Parent('mgs', 0, 'mul')]
+	);
+
+	g = new Quantity (
+		'Fallbeschleunigung',
+		'm/s²',
+		null,
+		[],
+		[new Parent('mgs', 0, 'mul')]
+	);
+		
+	s = new Quantity (
+		'Strecke',
+		'm',
+		null,
+		[],
+		[new Parent('mgs', 0, 'mul')]
+	);
+
+	l = new Quantity (
+		'Länge', 
+		'm',
+		null,
+		[],
+		[new Parent('F_el', 0, 'div2')]
+	);
+
+	q = new Quantity (
+		'',
+		'',
+		null,
+		[],
+		[new Parent('F_el', 1, 'mul')]
+	);
+		
+	mgs = new Quantity (
+		'm * g * s',
+		null,
+		null,
+		[new MulFormula('mgs', [m, g, s])],
+		[new Parent('F_el', 0, 'div1')]
+	);
+
+	E_el = new Quantity (
+		'Elektrische Feldstärke',
+		'N/C oder V/m',
+		null,
+		[],
+		[new Parent('F_el', 1, 'mul')]
+	);
+
+	F_el = new Quantity (
+		'elektrishce Feldkraft',
+		'N',
+		null,
+		[new DivFormula('F_el', mgs, l), new MulFormula('F_el', [E_el, q])],
+		[new Parent('W_el', 0)]
+	);
+
+	W_el = new Quantity (
+		'elektrische Energie',
+		'J',
+		null,
+		[new MulFormula('W_el', [F_el, s])],
+		[]	
+	);
+}
 	
-s = new Quantity (
-	'Strecke',
-	'm',
-	null,
-	[],
-	[new Parent('mgs', 0, 'mul')]
-);
 
-l = new Quantity (
-	'Länge', 
-	'm',
-	null,
-	[],
-	[new Parent('F_el', 0, 'div2')]
-);
-
-q = new Quantity (
-	'',
-	'',
-	null,
-	[],
-	[new Parent('F_el', 1, 'mul')]
-);
-	
-mgs = new Quantity (
-	'm * g * s',
-	null,
-	null,
-	[new MulFormula('mgs', [m, g, s])],
-	[new Parent('F_el', 0, 'div1')]
-);
-
-E_el = new Quantity (
-	'Elektrische Feldstärke',
-	'N/C oder V/m',
-	null,
-	[],
-	[new Parent('F_el', 1, 'mul')]
-);
-
-F_el = new Quantity (
-	'elektrishce Feldkraft',
-	'N',
-	null,
-	[new DivFormula('F_el', mgs, l), new MulFormula('F_el', [E_el, q])],
-	[new Parent('W_el', 0)]
-);
-
-W_el = new Quantity (
-	'elektrische Energie',
-	'J',
-	null,
-	[new MulFormula('W_el', [F_el, s])],
-	[]
-);
-	
-	
 function assignValues(given) {
 	for (var i in given) {
 		given[i][0].value = given[i][1];
@@ -244,76 +233,74 @@ function assignValues(given) {
 }
 
 
-function calcByPath() {
+function getSolution(given, searched) {
 	
-	let calc;
+	for (var i in given) {
 	
-	for (i = treePath.length - 1; i > 1; i--) {
+		if (given[i].formula != []) { 
 		
-		calc = treePath[i].calculate(eval(treePath[i - 1].parentQuantity));
-		treePath[i - 1].setParentValue(calc);
-	}
-	
-	return treePath[1].calculate(treePath[0]);
-}
+			for (var j in given[i][0].formula) { console.log(given[i][0].formula);
 
-function goTreeUp(searched) {
+				let givenFormula = given[i][0].formula[j];
+				let unknownSubQuantity = givenFormula.getUnknownSubQuantity();
 
-	if (searched.parents != []) { // hat die Größe Parents? falls nein -> unberechenbar
-		
-		for (var i in searched.parents) { // durch die Parents iterieren
+				if (givenFormula.unknownSubQuantities() == 1) {
+										
+					givenFormula.calculateAndSetValue(unknownSubQuantity);
+					givenArr.push([unknownSubQuantity, unknownSubQuantity.value]);
 
-			if (searched.parents[i].getConnectingFormula().onlyThisSubQuantityUnknown(searched)) { // wenn in der Parentformel nur die gesuchte Größe undefiniert ist
-
-				console.log(searched.name + ": true");
-				
-				treePath.push(searched.parents[i].getConnectingFormula()); // die Parentformel zu treePath hinzufügen
-				
-				if (eval(searched.parents[i].quantity).value != null) { // wenn der Wert des Parents definiert ist
-
-					return calcByPath(); // den gesuchten Wert nach dem treePath berechnen und returnen
-				} else {
-					
-					return goTreeUp(eval(searched.parents[i].quantity));
+					if (unknownSubQuantity == searched) {
+						return searched.value; 
+					}
 				}
-			} else { // wenn in der Parentformel noch andere undefinierte Größen vorhanden sind
+			}
+		} else {
 
-				let subQuantities = searched.parents[i].formula.subquantites;
+			if (given[i].parents != []) {
+
+				let givenParents = given[i].parents;
 				
-				for (var j in subQuantities) {
+				for (var j in givenParents) {
 
-					if (subQuantities[j] == null)
+					let givenParentFormula = givenParents[j].getConnectingFormula;
+					let unknownSubQuantity = givenParentsFormula.getUnknownSubQuantity();
+					
+					if (givenParents[j].quantity.value != null) {
 
-					return goTreeDown(subQuantities[j]);
+						if (givenParentFormula.unknownSubQuantities() == 1) {
+
+							givenParentFormula.calculateAndSetValue(unknownSubQuantity);
+							givenArr.push([unknownSubQuantity, unknownSubQuantity.value]);
+
+							if (unknownSubQuantity == searched) {
+								return searched.value;
+							}
+						}
+					} else {
+
+						if (givenParentFormula.unknownSubQuantities() == 0) {
+
+							givenParentFormula.calculateAndSetValue(givenParentFormula.parentQuantity);
+							givenArr.push([givenParentFormula.parentQuantity, givenParentFormula.parentQuantity.value]);
+
+							if (givenParentFormula.parentQuantity == searched) {
+								return searched.value;
+							}
+						}
+					}
 				}
 			}
 		}
-		
-	} else { return 'calculation impossible'; } // -> unberechenbar
-}
-
-function goTreeDown(searched) {
-
-
-}
-
-function getSolution(given, searched) {
-	
-	assignValues(given);
-	
-	treePath.push(searched);
-	
-	for (var i in searched.formula) {
-		
-		if (searched.formula[i].allSubQuantitiesKnown()) {
-			return searched.formula[i].calculate(searched);
-		}git
 	}
-	
-	return goTreeUp(searched);
 }
 
 
 // console.log('solution: ' + getSolution([[m, 2], [g, 3], [s, - 4]], mgs));
 
-console.log('solution: ' + getSolution([[m, 2], [g, 3], [s, 4], [F_el, 5]], l));
+
+let givenArr = [[m, 2], [g, 3], [s, 4], [F_el, 5]];
+let searched = l;
+
+assignValues(givenArr);
+
+console.log('solution: ' + getSolution(givenArr, searched));
